61101cad5be7bd668f2542858cc540bf13bfbcc1
José Sebastian López Ibarra <99367521+SebastianLopez43@users.noreply.github.com>
2022-08-18 09:47:32 -0600

    Add files via upload
    Práctica 1_Stack

---
+++ EVA1_1_STACK.java
@@ -0,0 +1,26 @@
+package eva1_1_stack;
+
+/**
+ * @author José Sebastian López Ibarra
+ * Thursday August 18 2022
+ */
+
+public class EVA1_1_STACK {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args){
+        A();
+    }
+    
+    public static void A(){
+        B();
+    }
+    
+    public static void B(){
+        int i = 1;
+        double d = 10.0;
+    }
+}
