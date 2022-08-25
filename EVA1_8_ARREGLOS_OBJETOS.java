350d4dffc54552099e1952dcfc715e3878f38518
José Sebastian López Ibarra <99367521+SebastianLopez43@users.noreply.github.com>
2022-08-25 09:49:15 -0600

    Práctica 8_Arreglos_Objetos

---
+++ EVA1_8_ARREGLOS_OBJETOS.java
@@ -0,0 +1,44 @@
+package eva1_8_arreglos_objetos;
+
+/**
+ * @author José Sebastian López Ibarra
+ * Thursday August 25 2022
+ */
+
+public class EVA1_8_ARREGLOS_OBJETOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        // Objeto de arreglo creado
+        // Clase identificador = new Clase();
+        // Instanciar objeto
+        Prueba[] arrayPrueba = new Prueba[20];
+        
+        // Acceder a la posición de tipo Prueba
+        // Acceder a las propiedades del arreglo
+        // System.out.println("Valor de c = " + arrayPrueba.lenght);
+        
+        // Acceder a las propiedades de lo que está almacenado en el objeto
+        // System.out.println(arrayPrueba[10].c);
+        
+        for(int i = 0; i < arrayPrueba.length; i++) {
+            arrayPrueba[i] = new Prueba();
+        }
+        
+        // Leer "c"
+        for (Prueba prueba : arrayPrueba) {
+            // Distintas direcciones - Diferentes objetos
+            System.out.println(prueba);
+            
+            // Contenido de cada objeto
+            System.out.println(prueba.c);
+        }
+    }    
+}
+
+class Prueba {
+    int c = 100;
+}
