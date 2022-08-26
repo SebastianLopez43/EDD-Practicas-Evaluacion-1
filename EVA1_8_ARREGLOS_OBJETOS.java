c753f73e2f3e83bddaca9aae9442875ba8b00daa
José Sebastian López Ibarra <99367521+SebastianLopez43@users.noreply.github.com>
2022-08-26 09:52:49 -0600

    Práctica 8_Arreglos_Objetos

--- EVA1_8_ARREGLOS_OBJETOS.java
+++ EVA1_8_ARREGLOS_OBJETOS.java
@@ -1,44 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package eva1_8_arreglos_objetos;
 
 /**
- * @author José Sebastian López Ibarra
- * Thursday August 25 2022
+ *
+ * @author moviles
  */
-
 public class EVA1_8_ARREGLOS_OBJETOS {
 
     /**
      * @param args the command line arguments
      */
-    
     public static void main(String[] args) {
-        // Objeto de arreglo creado
-        // Clase identificador = new Clase();
-        // Instanciar objeto
-        Prueba[] arrayPrueba = new Prueba[20];
-        
-        // Acceder a la posición de tipo Prueba
-        // Acceder a las propiedades del arreglo
-        // System.out.println("Valor de c = " + arrayPrueba.lenght);
-        
-        // Acceder a las propiedades de lo que está almacenado en el objeto
-        // System.out.println(arrayPrueba[10].c);
-        
-        for(int i = 0; i < arrayPrueba.length; i++) {
-            arrayPrueba[i] = new Prueba();
-        }
-        
-        // Leer "c"
-        for (Prueba prueba : arrayPrueba) {
-            // Distintas direcciones - Diferentes objetos
-            System.out.println(prueba);
+        // TODO code application logic here
+        Prueba[] arregloPrueba = new Prueba[20];
+        // crear los objetos
+        for (int i = 0; i < arregloPrueba.length; i++) {
+            // Clase identificador = new Clase();
+            arregloPrueba[i] = new Prueba();
             
-            // Contenido de cada objeto
-            System.out.println(prueba.c);
         }
-    }    
+         // leer x
+        for (Prueba prueba : arregloPrueba) {
+            System.out.println("[" + prueba.x + "]");
+            
+        }
+        
+    }
+    
 }
 
-class Prueba {
-    int c = 100;
+class Prueba{
+    int x = 100;
 }
