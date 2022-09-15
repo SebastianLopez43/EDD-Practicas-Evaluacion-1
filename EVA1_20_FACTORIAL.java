5db1d79c2f9983409c2076c625332e73f42a8119
José Sebastian López Ibarra <99367521+SebastianLopez43@users.noreply.github.com>
2022-09-15 09:53:57 -0600

    Práctica 20_Factorial

---
+++ EVA1_20_FACTORIAL.java
@@ -0,0 +1,17 @@
+package eva1_20_factorial;
+
+/**
+ * @author José Sebastian López Ibarra
+ * Thursday September 15 2022
+ */
+
+public class EVA1_20_FACTORIAL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
