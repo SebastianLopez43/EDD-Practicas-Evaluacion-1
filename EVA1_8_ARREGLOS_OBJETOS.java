3083d28bd48c26dd09665e2c84986a29dd059cb6
José Sebastian López Ibarra <99367521+SebastianLopez43@users.noreply.github.com>
2022-09-16 13:06:13 -0500

    Práctica 8_Arreglos_Objetos

--- EVA1_8_ARREGLOS_OBJETOS.java
+++ EVA1_8_ARREGLOS_OBJETOS.java
@@ -1,38 +1,51 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package eva1_8_arreglos_objetos;
 
 /**
- *
- * @author moviles
+ * @author José Sebastian López Ibarra
+ * Thursday August 25 2022
  */
+
 public class EVA1_8_ARREGLOS_OBJETOS {
 
     /**
      * @param args the command line arguments
      */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
-        Prueba[] arregloPrueba = new Prueba[20];
-        // crear los objetos
-        for (int i = 0; i < arregloPrueba.length; i++) {
-            // Clase identificador = new Clase();
-            arregloPrueba[i] = new Prueba();
-            
-        }
-         // leer x
-        for (Prueba prueba : arregloPrueba) {
-            System.out.println("[" + prueba.x + "]");
-            
+        
+        // Objeto de arreglo creado
+        // Clase identificador = new Clase();
+        // Instanciar objeto
+        Prueba[] arrayPrueba = new Prueba[20];
+        
+        // Acceder a la posición de tipo Prueba
+        // Acceder a las propiedades del arreglo
+        // System.out.println("Valor de c = " + arrayPrueba.lenght);
+        
+        // Acceder a las propiedades de lo que está almacenado en el objeto
+        // System.out.println(arrayPrueba[10].c);
+        
+        // Ciclo que crea los objetos
+        for(int i = 0; i < arrayPrueba.length; i++) {
+            arrayPrueba[i] = new Prueba();
         }
         
-    }
-    
+        System.out.println("Direcciones\n");
+        // For each que imprime las direcciones de cada objeto
+        for(Prueba prueba : arrayPrueba) {
+            // Distintas direcciones - Diferentes objetos
+            System.out.println(prueba);
+            }
+        
+        System.out.println("\nValores de cada objeto\n");
+        // For each que agrega el valor a cada objeto
+        for(Prueba prueba : arrayPrueba) {
+            // Contenido de cada objeto
+            System.out.println(prueba.c);
+        }
+    }    
 }
 
-class Prueba{
-    int x = 100;
+class Prueba {
+    int c = 100;
 }
