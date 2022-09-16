ca386ca04f143afaa02c62fbaac7d1a7d95d50ee
José Sebastian López Ibarra <99367521+SebastianLopez43@users.noreply.github.com>
2022-09-16 13:38:39 -0500

    Práctica 14_Paso_Ref

--- EVA1_14_PASO_REF.java
+++ EVA1_14_PASO_REF.java
@@ -16,19 +16,19 @@ public class EVA1_14_PASO_REF {
         // Creamos arreglo de enteros
         int[] arregloDatos = new int[100];
         
-        // Función que llena el arreglo con números aleatorios
-        llenar(arregloDatos);
-        
-        // Función que imprime el arreglo
+        // Llamadas a las funciones llenar e imprimir
+        llenar(arregloDatos);                
         imprimir(arregloDatos);
     }    
     
+    // Función que llena el arreglo con números aleatorios entre 1 y 100
     public static void llenar(int[] arreglo) {        
         for (int i = 0; i < arreglo.length; i++) {
             arreglo[i] = (int)(Math.random() * 100);
         }
     }
     
+    // Función que imprime el arreglo
     public static void imprimir(int[] arreglo) {
         for (int i = 0; i < arreglo.length; i++) {
             System.out.println("Valor " + (i+1) + " :[ " + arreglo[i] + " ]");
